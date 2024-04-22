e6ed91675ff925cde6896d3f2928ca57f481a7fb
Moaz-Maher <167722151+Moaz-Maher@users.noreply.github.com>
2024-04-22 11:56:04 +0200

    EcommerceSystemGUI.java

--- EcommerceSystemGUI.java
+++ EcommerceSystemGUI.java
@@ -1,140 +1,65 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JOptionPane;
+public class EcommerceSystem {
+    public static void main(String[] args) throws Exception {
 
-public class EcommerceSystemGUI extends JFrame {
-    private JTextField nameField, addressField, customerIdField, numProductsField;
-    private JButton submitButton, addProductButton;
-    private JLabel nameLabel, addressLabel, customerIdLabel, numProductsLabel;
-    private JList<String> productList;
-    private DefaultListModel<String> productListModel;
-    private int numSelectedProducts, numProducts;
-    private Cart cart;
+        ElectronicProduct electronicProduct = new ElectronicProduct();
+        electronicProduct.setName("smartphone");
+        electronicProduct.setProductId(1);
+        electronicProduct.setPrice(599.9f);
+        electronicProduct.setBrand("Samsung");
+        electronicProduct.setWarrantyPeriod(1);
 
-    public EcommerceSystemGUI() {
-        setTitle("E-commerce System");
-        setSize(500, 300);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-        JPanel inputPanel = new JPanel(new GridLayout(5, 2));
-        nameLabel = new JLabel("Enter your name");
-        nameField = new JTextField();
-        addressLabel = new JLabel("Enter your address");
-        addressField = new JTextField();
-        customerIdLabel = new JLabel("Enter your id");
-        customerIdField = new JTextField();
-        numProductsLabel = new JLabel("Enter number of products");
-        numProductsField = new JTextField();
-        numProductsField.addFocusListener(new FocusAdapter() {
-            public void focusLost(FocusEvent e) {
-                if (!numProductsField.getText().isEmpty()) {
-                    numProducts = Integer.parseInt(numProductsField.getText());
-                    addProductButton.setEnabled(true);
-                }
-            }
-        });
-        inputPanel.add(nameLabel);
-        inputPanel.add(nameField);
-        inputPanel.add(addressLabel);
-        inputPanel.add(addressField);
-        inputPanel.add(customerIdLabel);
-        inputPanel.add(customerIdField);
-        inputPanel.add(numProductsLabel);
-        inputPanel.add(numProductsField);
+        ClothingProduct clothingProduct = new ClothingProduct();
+        clothingProduct.setName("T-shirt");
+        clothingProduct.setProductId(2);
+        clothingProduct.setPrice(19.99f);
+        clothingProduct.setSize("Medium");
+        clothingProduct.setFabric("Cotton");
 
-        JPanel productListPanel = new JPanel(new BorderLayout());
-        productListModel = new DefaultListModel<>();
-        productList = new JList<>(productListModel);
-        productListPanel.add(new JScrollPane(productList), BorderLayout.CENTER);
+        BookProduct bookProduct = new BookProduct();
+        bookProduct.setName("OOP");
+        bookProduct.setProductId(3);
+        bookProduct.setPrice(39.99f);
+        bookProduct.setAuthor("O'Reilly");
+        bookProduct.setPublisher("X Publications");
 
-        JPanel productButtonPanel = new JPanel(new FlowLayout());
-        addProductButton = new JButton("Add Product");
-        addProductButton.setEnabled(false);
-        addProductButton.addActionListener(e -> {
-            String selectedProduct = (String) JOptionPane.showInputDialog(null, "Select a product", "Product Selection", JOptionPane.QUESTION_MESSAGE, null, new String[]{"Smartphone", "T-shirt", "Book"}, "Smartphone");
-            if (selectedProduct != null && numSelectedProducts < numProducts) {
-                productListModel.addElement(selectedProduct);
-                numSelectedProducts++;
-                if (numSelectedProducts == numProducts) {
-                    addProductButton.setEnabled(false);
-                }
-            }
-        });
-        productButtonPanel.add(addProductButton);
-        productListPanel.add(productButtonPanel, BorderLayout.SOUTH);
+        Customer customer = new Customer();
+        customer.setName(JOptionPane.showInputDialog(null, "Enter your name"));
+        customer.setAddress(JOptionPane.showInputDialog(null, "Enter your address"));
+        int customerId = Integer.parseInt(JOptionPane.showInputDialog("Enter your id"));
+        customer.setCustomerId(customerId);
 
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        mainPanel.add(inputPanel, BorderLayout.NORTH);
-        mainPanel.add(productListPanel, BorderLayout.CENTER);
-
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        submitButton = new JButton("Submit");
-        submitButton.addActionListener(e -> {
-            createCart();
-            displayOrderSummary();
-        });
-        buttonPanel.add(submitButton);
-
-        add(mainPanel, BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
-
-        numSelectedProducts = 0;
-        setVisible(true);
-    }
-
-    private void createCart() {
-        cart = new Cart();
-        cart.setCustomerId(Integer.parseInt(customerIdField.getText()));
-        cart.setnProducts(numProducts);
+        Cart cart = new Cart();
+        int nProducts = Integer.parseInt(JOptionPane.showInputDialog("How many products you want to add to your cart?"));
+        cart.setnProducts(nProducts);
         cart.setProducts();
-        for (int i = 0; i < productListModel.getSize(); i++) {
-            String product = productListModel.getElementAt(i);
-            switch (product) {
-                case "Smartphone":
-                    ElectronicProduct smartphone = new ElectronicProduct();
-                    smartphone.setName("Smartphone");
-                    smartphone.setPrice(599.9f);
-                    cart.addProduct(smartphone);
-                    break;
-                case "T-shirt":
-                    ClothingProduct tShirt = new ClothingProduct();
-                    tShirt.setName("T-shirt");
-                    tShirt.setPrice(19.99f);
-                    cart.addProduct(tShirt);
-                    break;
-                case "Book":
-                    BookProduct book = new BookProduct();
-                    book.setName("Book");
-                    book.setPrice(39.99f);
-                    cart.addProduct(book);
-                    break;
+        System.out.print(electronicProduct.getProductId() + "- " + electronicProduct.getName() + "\t");
+        System.out.print(clothingProduct.getProductId() + "- " + clothingProduct.getName() + "\t");
+        System.out.println(bookProduct.getProductId() + "- " + bookProduct.getName());
+        while (nProducts > 0) {
+            int choice = Integer.parseInt(JOptionPane.showInputDialog(electronicProduct.getProductId() + "- " + electronicProduct.getName() + "\n" + clothingProduct.getProductId() + "- " + clothingProduct.getName() + "\n" + bookProduct.getProductId() + "- " + bookProduct.getName() + "\n" + "Which product do you want to to add to your cart?"));
+            if (choice == electronicProduct.productId) {
+                cart.addProduct(electronicProduct);
+            } else if (choice == clothingProduct.getProductId()) {
+                cart.addProduct(clothingProduct);
+            } else if (choice == bookProduct.getProductId()) {
+                cart.addProduct(bookProduct);
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid option");
+                continue;
             }
+            nProducts--;
         }
-    }
 
-    private void displayOrderSummary() {
-        StringBuilder message = new StringBuilder();
-        message.append("<html><body>");
-        message.append("<h2 style='text-align:center;'>Order Summary</h2>");
-        message.append("<p><b>Name:</b> ").append(nameField.getText()).append("</p>");
-        message.append("<p><b>Address:</b> ").append(addressField.getText()).append("</p>");
-        message.append("<p><b>Customer ID:</b> ").append(customerIdField.getText()).append("</p>");
-        message.append("<p><b>Total Products:</b> ").append(numSelectedProducts).append("</p>");
-        message.append("<p><b>Selected Products:</b></p>");
-        message.append("<ul>");
-        float totalPrice = 0.0f;
-        for (Product product : cart.getProducts()) {
-            message.append("<li>").append(product.getName()).append(" - $").append(product.getPrice()).append("</li>");
-            totalPrice += product.getPrice();
+        Order order = new Order();
+        order.setCustomerId(customerId);
+        order.setOrderId();
+        order.setProducts(cart.getProducts());
+        order.setTotalPrice(cart.calculatePrice());
+        if (cart.placeOrder(JOptionPane.showInputDialog("Your total price is: $" + cart.calculatePrice() + "\n" + "Would you like to placeyour order for the products in the cart?(Y/N)"))) {
+            order.printOrderInfo();
+        } else {
+            JOptionPane.showMessageDialog(null, "Your order has been canceled");
         }
-        message.append("</ul>");
-        message.append("<p><b>Total Price:</b> $").append(totalPrice).append("</p>");
-        message.append("</body></html>");
-
-        JOptionPane.showMessageDialog(null, message.toString());
-    }
-
-    public static void main(String[] args) {
-        new EcommerceSystemGUI();
     }
 }
